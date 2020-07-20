4019e296ac1e58c88a8b20e3f97b5fb285ac36bc
zuozqi <mrzuozhongqi@gmail.com>
2020-07-20 15:29:58 +0800

    Add heat flux utilities

---
+++ Utilities/wallHeatFluxVapor/wallHeatFluxVapor.C
@@ -0,0 +1,312 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2016-2017 OpenFOAM Foundation
+    Copyright (C) 2016-2018 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "wallHeatFluxVapor.H"
+#include "turbulentFluidThermoModel.H"
+#include "solidThermo.H"
+#include "surfaceInterpolate.H"
+#include "fvcSnGrad.H"
+#include "wallPolyPatch.H"
+#include "turbulentFluidThermoModel.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace functionObjects
+{
+    defineTypeNameAndDebug(wallHeatFluxVapor, 0);
+    addToRunTimeSelectionTable(functionObject, wallHeatFluxVapor, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * //
+
+void Foam::functionObjects::wallHeatFluxVapor::writeFileHeader(Ostream& os) const
+{
+    // Add headers to output data
+    writeHeader(os, "Wall heat-flux");
+    writeCommented(os, "Time");
+    writeTabbed(os, "patch");
+    writeTabbed(os, "min");
+    writeTabbed(os, "max");
+    writeTabbed(os, "integral");
+    os  << endl;
+}
+
+
+void Foam::functionObjects::wallHeatFluxVapor::calcHeatFlux
+(
+    const volScalarField& alpha,
+    const volScalarField& he,
+    volScalarField& wallHeatFluxVapor
+)
+{
+    volScalarField::Boundary& wallHeatFluxVaporBf = wallHeatFluxVapor.boundaryFieldRef();
+
+    const volScalarField::Boundary& heBf = he.boundaryField();
+
+    const volScalarField::Boundary& alphaBf = alpha.boundaryField();
+
+    forAll(wallHeatFluxVaporBf, patchi)
+    {
+        if (!wallHeatFluxVaporBf[patchi].coupled())
+        {
+            wallHeatFluxVaporBf[patchi] = alphaBf[patchi]*heBf[patchi].snGrad();
+        }
+    }
+
+    if (foundObject<volScalarField>(qrName_))
+    {
+        const volScalarField& qr = lookupObject<volScalarField>(qrName_);
+
+        const volScalarField::Boundary& radHeatFluxBf = qr.boundaryField();
+
+        forAll(wallHeatFluxVaporBf, patchi)
+        {
+            if (!wallHeatFluxVaporBf[patchi].coupled())
+            {
+                wallHeatFluxVaporBf[patchi] -= radHeatFluxBf[patchi];
+            }
+        }
+        const volScalarField alphal = scalar(1) - mesh_.lookupObject<volScalarField>("alpha.liquid");
+        const volScalarField::Boundary& alphalbf = alphal.boundaryField();
+        forAll(wallHeatFluxVaporBf, patchi)
+        {
+            wallHeatFluxVaporBf[patchi] = wallHeatFluxVaporBf[patchi]*alphalbf[patchi];
+        }
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::functionObjects::wallHeatFluxVapor::wallHeatFluxVapor
+(
+    const word& name,
+    const Time& runTime,
+    const dictionary& dict
+)
+:
+    fvMeshFunctionObject(name, runTime, dict),
+    writeFile(obr_, name, typeName, dict),
+    patchSet_(),
+    qrName_("qr")
+{
+    volScalarField* wallHeatFluxVaporPtr
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                type(),
+                mesh_.time().timeName(),
+                mesh_,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            mesh_,
+            dimensionedScalar(dimMass/pow3(dimTime), Zero)
+        )
+    );
+
+    mesh_.objectRegistry::store(wallHeatFluxVaporPtr);
+
+    read(dict);
+
+    writeFileHeader(file());
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::functionObjects::wallHeatFluxVapor::~wallHeatFluxVapor()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+bool Foam::functionObjects::wallHeatFluxVapor::read(const dictionary& dict)
+{
+    fvMeshFunctionObject::read(dict);
+    writeFile::read(dict);
+
+    const polyBoundaryMesh& pbm = mesh_.boundaryMesh();
+
+    patchSet_ =
+        mesh_.boundaryMesh().patchSet
+        (
+            wordReList(dict.lookupOrDefault("patches", wordReList()))
+        );
+
+    dict.readIfPresent("qr", qrName_);
+
+    Info<< type() << " " << name() << ":" << nl;
+
+    if (patchSet_.empty())
+    {
+        forAll(pbm, patchi)
+        {
+            if (isA<wallPolyPatch>(pbm[patchi]))
+            {
+                patchSet_.insert(patchi);
+            }
+        }
+
+        Info<< "    processing all wall patches" << nl << endl;
+    }
+    else
+    {
+        Info<< "    processing wall patches: " << nl;
+        labelHashSet filteredPatchSet;
+        for (const label patchi : patchSet_)
+        {
+            if (isA<wallPolyPatch>(pbm[patchi]))
+            {
+                filteredPatchSet.insert(patchi);
+                Info<< "        " << pbm[patchi].name() << endl;
+            }
+            else
+            {
+                WarningInFunction
+                    << "Requested wall heat-flux on non-wall boundary "
+                    << "type patch: " << pbm[patchi].name() << endl;
+            }
+        }
+
+        Info<< endl;
+
+        patchSet_ = filteredPatchSet;
+    }
+
+    return true;
+}
+
+
+bool Foam::functionObjects::wallHeatFluxVapor::execute()
+{
+    volScalarField& wallHeatFluxVapor = lookupObjectRef<volScalarField>(type());
+
+    if
+    (
+        foundObject<compressible::turbulenceModel>
+        (
+            turbulenceModel::propertiesName
+        )
+    )
+    {
+        const compressible::turbulenceModel& turbModel =
+            lookupObject<compressible::turbulenceModel>
+            (
+                turbulenceModel::propertiesName
+            );
+
+        calcHeatFlux
+        (
+            turbModel.alphaEff()(),
+            turbModel.transport().he(),
+            wallHeatFluxVapor
+        );
+    }
+    else if (foundObject<fluidThermo>(fluidThermo::dictName))
+    {
+        const fluidThermo& thermo =
+            lookupObject<fluidThermo>(fluidThermo::dictName);
+
+        calcHeatFlux
+        (
+            thermo.alpha(),
+            thermo.he(),
+            wallHeatFluxVapor
+        );
+    }
+    else if (foundObject<solidThermo>(solidThermo::dictName))
+    {
+        const solidThermo& thermo =
+            lookupObject<solidThermo>(solidThermo::dictName);
+
+        calcHeatFlux(thermo.alpha(), thermo.he(), wallHeatFluxVapor);
+    }
+    else
+    {
+        FatalErrorInFunction
+            << "Unable to find compressible turbulence model in the "
+            << "database" << exit(FatalError);
+    }
+
+    return true;
+}
+
+
+bool Foam::functionObjects::wallHeatFluxVapor::write()
+{
+    const volScalarField& wallHeatFluxVapor = lookupObject<volScalarField>(type());
+
+    Log << type() << " " << name() << " write:" << nl
+        << "    writing field " << wallHeatFluxVapor.name() << endl;
+
+    wallHeatFluxVapor.write();
+
+    const fvPatchList& patches = mesh_.boundary();
+
+    const surfaceScalarField::Boundary& magSf =
+        mesh_.magSf().boundaryField();
+
+    for (const label patchi : patchSet_)
+    {
+        const fvPatch& pp = patches[patchi];
+
+        const scalarField& hfp = wallHeatFluxVapor.boundaryField()[patchi];
+
+        const scalar minHfp = gMin(hfp);
+        const scalar maxHfp = gMax(hfp);
+        const scalar integralHfp = gSum(magSf[patchi]*hfp);
+
+        if (Pstream::master())
+        {
+            writeCurrentTime(file());
+
+            file()
+                << token::TAB << pp.name()
+                << token::TAB << minHfp
+                << token::TAB << maxHfp
+                << token::TAB << integralHfp
+                << endl;
+        }
+
+        Log << "    min/max/integ(" << pp.name() << ") = "
+            << minHfp << ", " << maxHfp << ", " << integralHfp << endl;
+    }
+
+    return true;
+}
+
+
+// ************************************************************************* //
